eb4003746545600a721a55bd3cf02f08507a53b5
Vladimir <chyvikovva@gmail.com>
2019-10-31 22:28:24 +0300

    Update conf.py

--- conf.py
+++ conf.py
@@ -20,7 +20,7 @@ import os
 import sys
 
 
-sys.path.insert(0, os.path.abspath('.'))
+#sys.path.insert(0, os.path.abspath('.'))
 
 # -- General configuration ------------------------------------------------
 
@@ -31,7 +31,7 @@ sys.path.insert(0, os.path.abspath('.'))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc']
+#extensions = ['sphinx.ext.autodoc']
 
 templates_path = ['_templates']
 
